3e7044d06064195f7603094fd1f41cfa17f9695c
TimeIsMoney01 <albertbracic1@gmail.com>
2025-06-29 16:29:10 -0500

    Request notification permissions on launch

--- Time_is_MoneyApp.swift
+++ Time_is_MoneyApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
+/// The main entry point of the app. Requests notification permissions on launch.
 @main
 struct Time_is_MoneyApp: App {
+    init() {
+        NotificationManager.shared.requestAuthorization()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
